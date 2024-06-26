1d8d2b2e2545e6a3a7e50a403370760b1007c60a
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-26 17:25:03 +0300

    users controller

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Get(':id')
+  getById(@Param() id: string) {
+    return this.userService.getById(id);
+  }
+
+  @Get()
+  getAll() {
+    return this.userService.getAll();
+  }
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.userService.create(dto);
+  }
+
+  @Patch(':id')
+  update(@Param() id: string, @Body() dto: UpdateUserDto) {
+    return this.userService.update(id, dto);
+  }
+
+  @Delete(':id')
+  delete(@Param() id: string) {
+    return this.userService.delete(id);
+  }
+}
