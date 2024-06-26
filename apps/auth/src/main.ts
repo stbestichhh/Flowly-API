89bb3bd2582f4efe26b45690cef58d8d2c513cd7
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-26 17:47:29 +0300

    configured swagger

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Authorization')
+    .setDescription('Flowly authorization microservice API')
+    .setVersion('1.0.0')
+    .addTag('Flowly API')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('/api/docs', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
