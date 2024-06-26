be73634aae04dc234849e0b0f3de8a973c8dc882
Kiril Yakymchuk <stbestich@gmail.com>
2024-06-26 17:51:40 +0300

    documented craete user dto

--- apps/auth/src/users/dto/create-user.dto.ts
+++ apps/auth/src/users/dto/create-user.dto.ts
@@ -5,23 +5,27 @@ import {
   IsStrongPassword,
   Length,
 } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty({ example: 'BravePlant', description: 'Username' })
   @IsNotEmpty()
-  @IsString({ message: 'Has to be string.' })
+  @IsString({ message: 'Has to be string' })
   @Length(3)
   username: string;
 
+  @ApiProperty({ example: 'brave_plant@garden.com', description: 'Email' })
   @IsNotEmpty()
-  @IsString({ message: 'Has to be string.' })
-  @IsEmail({}, { message: 'Wrong email format.' })
+  @IsString({ message: 'Has to be string' })
+  @IsEmail({}, { message: 'Wrong email format' })
   email: string;
 
+  @ApiProperty({ example: 'S1rongPa6wor?', description: 'Password' })
   @IsNotEmpty()
-  @IsString({ message: 'Has to be string.' })
-  @IsStrongPassword({}, { message: 'Password is not strong enough.' })
+  @IsString({ message: 'Has to be string' })
+  @IsStrongPassword({}, { message: 'Password is not strong enough' })
   @Length(4, 16, {
-    message: 'Password length has to be between 4 and 16 characters length.',
+    message: 'Password length has to be between 4 and 16 characters length',
   })
   password: string;
 }
--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -8,6 +8,7 @@ import {
   Post,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto';
 
 @Controller('users')
 export class UsersController {
