573171113e60ecf4fe00669ce2f561a8c46a434d
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-01 13:15:29 +0300

    prividede custom logger for sequelize module

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,17 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { Role, User, UserRole } from '@app/common/database/models';
 import * as process from 'process';
+import * as Joi from 'joi';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import { LoggerModule } from '@app/common/logger';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `.${process.env.NODE_ENV}.env`,
       isGlobal: true,
+      validationSchema: Joi.object({
+        POSTGRES_HOST: Joi.string().hostname().required(),
+        POSTGRES_PORT: Joi.number().port().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+      }),
     }),
     SequelizeModule.forRootAsync({
-      useFactory: (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService, logger: PinoLogger) => ({
         dialect: 'postgres',
         host: configService.get('POSTGRES_HOST'),
         port: Number(configService.get('POSTGRES_PORT')),
@@ -21,14 +31,15 @@ import * as process from 'process';
         models: [User, Role, UserRole],
         autoLoadModels: true,
         sync: { alter: true, force: false },
+        logging: (msg) => logger.log(msg, SequelizeModule.name),
       }),
-      inject: [ConfigService],
-      imports: [ConfigModule],
+      inject: [ConfigService, PinoLogger],
+      imports: [ConfigModule, LoggerModule],
     }),
   ],
 })
 export class DatabaseModule {
-  static forFeatue(entities: any[]) {
+  static forFeature(entities: any[]) {
     return SequelizeModule.forFeature(entities);
   }
 }
