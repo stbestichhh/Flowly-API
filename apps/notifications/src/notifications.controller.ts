8e2408d3a8bd825136f7659e49275d3d21e815bf
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-12 16:55:21 +0300

    added event pattern

--- apps/notifications/src/notifications.controller.ts
+++ apps/notifications/src/notifications.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { Payload } from '@nestjs/microservices';
+import { EventPattern, Payload } from '@nestjs/microservices';
 import { EmailDto } from './dto';
 
 @Controller()
 export class NotificationsController {
   constructor(private readonly notificationsService: NotificationsService) {}
 
-  @Get()
+  @EventPattern('notify_email')
   public async sendEMail(@Payload() dto: EmailDto) {
     return await this.notificationsService.sendEmail(dto);
   }
