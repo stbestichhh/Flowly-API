614c6fcf9a593cafe6757ae79361f1e71ef13c3a
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-17 12:55:53 +0300

    defined endpoints in project controller

--- apps/flowly/src/project/project.controller.ts
+++ apps/flowly/src/project/project.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from '@app/common/decorators';
+import { RolesEnum } from '@app/common/enums';
+import { JwtGuard, RoleGuard } from '@app/common/guards';
+import { ProjectService } from './project.service';
 
+@ApiTags('Projects')
+@Roles(RolesEnum.PROJECT_MANAGER)
+@UseGuards(JwtGuard, RoleGuard)
+@ApiBearerAuth()
 @Controller('project')
-export class ProjectController {}
+export class ProjectController {
+  constructor(private readonly projectService: ProjectService) {}
+
+  @Get()
+  public async getAll() {
+    return await this.projectService.getAll();
+  }
+
+  @Get(':id')
+  public async getById() {
+    return await this.projectService.getById();
+  }
+
+  @Post()
+  public async create() {
+    return await this.projectService.create();
+  }
+
+  @Patch(':id')
+  public async update() {
+    return await this.projectService.update();
+  }
+
+  @Delete(':id')
+  public async delete() {
+    return await this.projectService.delete();
+  }
+}
--- apps/flowly/src/project/project.service.ts
+++ apps/flowly/src/project/project.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+  public async getAll() {}
+
+  public async getById() {}
+
+  public async create() {}
+
+  public async update() {}
+
+  public async delete() {}
+}
