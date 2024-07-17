b3e5cc090a95afcd9300a45d698584d71a3f8a05
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-17 13:26:31 +0300

    project module

--- apps/flowly/src/project/project.module.ts
+++ apps/flowly/src/project/project.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ProjectController } from './project.controller';
 import { ProjectService } from './project.service';
+import { DatabaseModule, Project } from '@app/common/database';
+import { RateLimitterModule } from '@app/common/rate-limitter';
+import { JwtStrategy } from '@app/common/strategies';
 
 @Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([Project]),
+    RateLimitterModule,
+  ],
   controllers: [ProjectController],
-  providers: [ProjectService],
+  providers: [ProjectService, JwtStrategy],
 })
 export class ProjectModule {}
