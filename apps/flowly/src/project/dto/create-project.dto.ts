9da66e427161454cec79df17d34ae365201cfa9b
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-18 13:31:04 +0300

    craete and update project dto

--- apps/flowly/src/project/dto/create-project.dto.ts
+++ apps/flowly/src/project/dto/create-project.dto.ts
@@ -1,11 +1,15 @@
 import { AbstractDto } from '@app/common/dto';
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateProjectDto extends AbstractDto {
+  @ApiProperty({ example: 'My Project', description: 'Project name' })
   @IsNotEmpty()
   @IsString({ message: 'Has to be string' })
+  @Length(4)
   readonly name: string;
 
+  @ApiProperty({ example: 'It is my best project', description: 'Project description' })
   @IsOptional()
   @IsNotEmpty()
   @IsString({ message: 'Has to be string' })
--- apps/flowly/src/project/dto/update-project.dto.ts
+++ apps/flowly/src/project/dto/update-project.dto.ts
@@ -1,4 +1,20 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateProjectDto } from './create-project.dto';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsOptional, IsString, Length } from 'class-validator';
 
-export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {
+  @ApiProperty({ example: 'My Project', description: 'Project name' })
+  @IsNotEmpty()
+  @IsString({ message: 'Has to be string' })
+  @Length(4)
+  @IsOptional()
+  readonly name?: string;
+
+  @ApiProperty({ example: 'It is my best project', description: 'Project description' })
+  @IsOptional()
+  @IsNotEmpty()
+  @IsString({ message: 'Has to be string' })
+  @IsOptional()
+  readonly description?: string;
+}
