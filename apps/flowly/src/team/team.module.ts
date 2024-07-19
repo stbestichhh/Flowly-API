8d2879d0171a7a27922d61b051429ec31cba2b31
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-19 18:59:58 +0300

    team module

--- apps/flowly/src/team/team.module.ts
+++ apps/flowly/src/team/team.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { DatabaseModule, Team } from '@app/common/database';
+import { TeamRepository } from './team.repository';
+import { ProjectModule } from '../project/project.module';
 
 @Module({
-  providers: [TeamService],
-  controllers: [TeamController]
+  providers: [TeamService, TeamRepository],
+  controllers: [TeamController],
+  imports: [
+    ProjectModule,
+    DatabaseModule.forFeature([Team])
+  ]
 })
 export class TeamModule {}
