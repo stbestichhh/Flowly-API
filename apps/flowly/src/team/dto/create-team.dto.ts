3c7a44051f967bb828a3bbd7ee1506bbabd28a8b
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-19 18:59:29 +0300

    create and update team dto

---
+++ apps/flowly/src/team/dto/create-team.dto.ts
@@ -0,0 +1,16 @@
+import { AbstractDto } from '@app/common/dto';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTeamDto extends AbstractDto {
+  @ApiProperty({ example: 'My team', description: 'Team name' })
+  @IsNotEmpty()
+  @IsString({ message: 'Has to be string' })
+  @Length(4)
+  readonly name: string;
+
+  @ApiProperty({ example: 'projectuuid', description: 'Project uuid to attach created team' })
+  @IsNotEmpty()
+  @IsString({ message: 'Has to be string' })
+  readonly projectId: string;
+}
