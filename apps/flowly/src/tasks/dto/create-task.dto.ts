9c501d19245b841ac5ab6402ed41eb4e1d5cc9c5
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-21 15:52:24 +0300

    create task dto

---
+++ apps/flowly/src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,22 @@
+import { AbstractDto } from '@app/common/dto';
+import { IsNotEmpty, IsOptional, IsString, IsUUID, Length } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class CreateTaskDto extends AbstractDto {
+  @ApiProperty({ example: 'My task', description: 'Task name' })
+  @IsString({ message: 'Has to be string' })
+  @IsNotEmpty()
+  @Length(4)
+  readonly name: string;
+
+  @ApiProperty({ example: 'The hardest task', description: 'Task description' })
+  @IsString({ message: 'Has to be string' })
+  @IsOptional()
+  readonly description?: string;
+
+  @ApiProperty({ example: 'projectuuid', description: 'Project id this task relates to' })
+  @IsString({ message: 'Has to be a string' })
+  @IsUUID()
+  @IsNotEmpty()
+  readonly projectId: string;
+}
