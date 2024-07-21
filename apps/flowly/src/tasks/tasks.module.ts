3bf3ff68ec5e03621313766e00a4286100e55c15
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-21 16:17:02 +0300

    updated task module imports

--- apps/flowly/src/tasks/tasks.module.ts
+++ apps/flowly/src/tasks/tasks.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { TasksController } from './tasks.controller';
+import { TaskRepository } from './task.repository';
+import { DatabaseModule, Task } from '@app/common/database';
 
 @Module({
-  providers: [TasksService],
-  controllers: [TasksController]
+  providers: [TasksService, TaskRepository],
+  controllers: [TasksController],
+  imports: [DatabaseModule.forFeature([Task])],
 })
 export class TasksModule {}
