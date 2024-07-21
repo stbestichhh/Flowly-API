8ac01901da43e235c27bcabd6560b97bb44950eb
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-21 16:25:20 +0300

    task service

--- apps/flowly/src/tasks/tasks.service.ts
+++ apps/flowly/src/tasks/tasks.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { TaskRepository } from './task.repository';
+import { CreateTaskDto, UpdateTaskDto } from './dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly taskRepository: TaskRepository) {}
+
+  public async getById(id: string) {
+    return await this.taskRepository.findByPk(id);
+  }
+
+  public async create(dto: CreateTaskDto) {
+    return await this.taskRepository.create(dto);
+  }
+
+  async update(id: string, dto: UpdateTaskDto) {
+    return await this.taskRepository.update(id, dto);
+  }
+
+  public async completeTask(id: string) {
+    return await this.taskRepository.update(id, { completed: true });
+  }
+
+  async delete(id: string) {
+    return await this.taskRepository.delete(id);
+  }
+}
