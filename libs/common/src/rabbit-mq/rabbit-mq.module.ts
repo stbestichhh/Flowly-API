75c8a1c39a49565d025d096632b5eb333797a698
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-28 13:21:28 +0300

    rabbitmq module register async

--- libs/common/src/rabbit-mq/rabbit-mq.module.ts
+++ libs/common/src/rabbit-mq/rabbit-mq.module.ts
@@ -1,4 +1,41 @@
 import { Module } from '@nestjs/common';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { RabbitMqService } from '@app/common/rabbit-mq/rabbit-mq.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as process from 'process';
+import * as Joi from 'joi';
 
-@Module({})
-export class RabbitMqModule {}
+@Module({
+  imports: [
+    ClientsModule,
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        RABBITMQ_URL: Joi.string().required(),
+      }),
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+  ],
+  providers: [RabbitMqService],
+  exports: [RabbitMqService],
+})
+export class RabbitMqModule {
+  public static registerAsync(name: string, queue: string) {
+    ClientsModule.registerAsync([
+      {
+        name,
+        useFactory: (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [configService.get<string>('RABBITMQ_URL')],
+            queue,
+            queueOptions: {
+              durable: false,
+            },
+          },
+        }),
+        imports: [ConfigModule],
+        inject: [ConfigService],
+      },
+    ]);
+  }
+}
