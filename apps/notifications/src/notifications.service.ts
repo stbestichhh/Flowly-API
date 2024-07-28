25e64662f0a6723df2ac5e7eb137e4cea45330bb
Kiril Yakymchuk <stbestich@gmail.com>
2024-07-28 13:39:22 +0300

    added logging notifications

--- apps/notifications/src/notifications.service.ts
+++ apps/notifications/src/notifications.service.ts
@@ -1,20 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { EmailDto } from './dto';
 import { MailerService } from '@nestjs-modules/mailer';
 import * as path from 'path';
 
 @Injectable()
 export class NotificationsService {
+  private readonly logger = new Logger(NotificationsService.name);
+
   constructor(private readonly mailService: MailerService) {}
 
   public async sendEmail(dto: EmailDto) {
-    return await this.mailService.sendMail({
+    await this.mailService.sendMail({
       to: dto.receiver,
       subject: dto.subject,
       template: path.resolve('libs/common/src/email/templates/template.hbs'),
       context: {
         message: dto.message,
       },
+    }).then(() => {
+      this.logger.log({ status: 'OK', data: dto });
     });
   }
 }
